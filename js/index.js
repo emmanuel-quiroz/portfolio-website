eedc8b2a271f3adda1ea9e2eb0167d1bfe919bf3
Emmanuel Gil <gilemmanuel12@gmail.com>
2022-03-04 12:00:57 -0800

    Added skills section

--- js/index.js
+++ js/index.js
@@ -1,36 +1,3 @@
 //add shadow to navbar on scroll down 
 
 
-
-//animate icons by adding animated classes
-function animateIcons () {
-    //Programming languages 
-    var iconsListLang = document.querySelectorAll(".lang div");
-    iconsListLang[0].classList.add("animated" , "fadeInDown");
-    iconsListLang[1].classList.add("animated" , "fadeInRight");
-    iconsListLang[2].classList.add("animated", "fadeInUp");
-    iconsListLang[3].classList.add("animated", "fadeInRight");
-
-    //Frameworks 
-    var iconsListFrameworks = document.querySelectorAll(".frame div");
-    iconsListFrameworks[0].classList.add("animated" , "fadeInLeft");
-    iconsListFrameworks[1].classList.add("animated" , "fadeInRight");
-    iconsListFrameworks[2].classList.add("animated" , "fadeInRight");
-    iconsListFrameworks[3].classList.add("animated" , "fadeInRight");
-
-    //Tools 
-    var iconsListTools = document.querySelectorAll(".tools div");
-    iconsListTools[0].classList.add("animated" , "fadeInLeft");
-    iconsListTools[1].classList.add("animated" , "fadeInRight");
-    iconsListTools[2].classList.add("animated", "fadeInDown");
-    iconsListTools[3].classList.add("animated", "fadeInRight");
-
-
-}
-
-animateIcons();
-
-
-
-
-console.log("All tests passed.");
